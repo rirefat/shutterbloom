fea859d712b506b7e6b727b987a44fd68ffbe9f6
rirefat <rirefat.official@gmail.com>
2024-08-23 01:29:46 +0600

    update image data and solve component class issues

--- components/PhotoList.jsx
+++ components/PhotoList.jsx
@@ -3,7 +3,7 @@ import PhotoCard from "./PhotoCard";
 
 const PhotoList = ({ photos }) => {
     return (
-        <div class="img-grid">
+        <div className="img-grid">
             {
                 photos.map((photo) => <PhotoCard key={photo.id} photo={photo} />)
             }
